3885ab2840af6b6041079545593d34d08796b433
Ramom Silva <ra-mom2000@hotmail.com>
2021-10-25 16:36:53 -0300

    create home view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import HomeView from './views/Home.view';
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeView></HomeView>
   </React.StrictMode>,
   document.getElementById('root')
 );
